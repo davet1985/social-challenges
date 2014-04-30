91de2f78481f1acba0efc52ae4a9b0f41fccc54f
somme <s.sakounthong@kainos.com>
2014-04-30 15:37:40 +0100

    modified server.js

--- frontend/app/server.js
+++ frontend/app/server.js
@@ -10,11 +10,15 @@ var express = require('express');
 var app = express();
 port = process.argv[2] || 8000;
 
-app.configure(function () {
-    app.use(
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+       app.use(
         "/", //the URL throught which you want to access to you static content
         express.static(__dirname) //where your static content is located in your filesystem
     );
-});
+}
+
 app.listen(port); //the port you want to use
 console.log("Express server running at => http://localhost:" + port + "/\nCTRL + C to shutdown");
+
+
