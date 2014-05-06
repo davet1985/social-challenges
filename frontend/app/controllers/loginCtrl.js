50e3058319226d293c6a89d55e49613d942f5d41
Dave Thompson <davethompson21@gmail.com>
2014-05-06 20:19:27 +0100

    Semi-working login from front-end

--- frontend/app/controllers/loginCtrl.js
+++ frontend/app/controllers/loginCtrl.js
@@ -1,13 +1,10 @@
 var loginCtrl = function ($scope, $http) {
 
-    $scope.attemptLogin = function() {
-        console.log('start');
-
+    $scope.processForm = function() {
         $http({
-            url: 'http://localhost:9393/auth/login',
-            method: 'POST',
-            data: {'username': 'davethompson21@gmail.com', 'password': 'password12!'},
-            // data: xsrf,
+            method  : 'POST',
+            url     : 'http://localhost:9393/auth/login',
+            data: {'username': $scope.username, 'password': $scope.password},
             transformRequest: function(obj) {
                 var str = [];
                 for(var p in obj) {
@@ -15,16 +12,20 @@ var loginCtrl = function ($scope, $http) {
                 }
                 return str.join('&');
             },
-            // data: $.param($scope.formData),
-            headers : { 'Content-Type': 'application/x-www-form-urlencoded', 'Access-Control-Allow-Origin': '*' }
-        }).success(function(data, status, headers, config) {
-            console.log('success');
-            $scope.data = data;
-        }).error(function(data, status, headers, config) {
-            console.log('error');
-            $scope.status = status;
-        });
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+        })
+            .success(function(data) {
+                console.log(data);
 
+                // if (!data.success) {
+                //   // if not successful, bind errors to error variables
+                //     $scope.errorName = data.errors.name;
+                //     $scope.errorSuperhero = data.errors.superheroAlias;
+                // } else {
+                //   // if successful, bind success message to message
+                //     $scope.message = data.message;
+                // }
+            });
     };
 
 };
