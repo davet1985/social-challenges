532c1f8185f86b41930d22570835367da98bf95a
Dave Thompson <davethompson21@gmail.com>
2014-05-06 19:44:49 +0100

    login form post WIP

--- frontend/app/controllers/loginCtrl.js
+++ frontend/app/controllers/loginCtrl.js
@@ -1,17 +1,27 @@
 var loginCtrl = function ($scope, $http) {
 
     $scope.attemptLogin = function() {
+        console.log('start');
 
         $http({
             url: 'http://localhost:9393/auth/login',
             method: 'POST',
-            data: {'foo':'bar'},
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }
+            data: {'username': 'davethompson21@gmail.com', 'password': 'password12!'},
+            // data: xsrf,
+            transformRequest: function(obj) {
+                var str = [];
+                for(var p in obj) {
+                    str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                }
+                return str.join('&');
+            },
+            // data: $.param($scope.formData),
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded', 'Access-Control-Allow-Origin': '*' }
         }).success(function(data, status, headers, config) {
+            console.log('success');
             $scope.data = data;
         }).error(function(data, status, headers, config) {
+            console.log('error');
             $scope.status = status;
         });
 
