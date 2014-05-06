36f77eccfff5e092fff333381b297b9a57cbb85f
Dave Thompson <davethompson21@gmail.com>
2014-05-06 19:03:31 +0100

    Login post WIP

--- frontend/app/controllers/loginCtrl.js
+++ frontend/app/controllers/loginCtrl.js
@@ -1,7 +1,22 @@
-var loginCtrl = function ($scope) {
+var loginCtrl = function ($scope, $http) {
 
+    $scope.attemptLogin = function() {
 
+        $http({
+            url: 'http://localhost:9393/auth/login',
+            method: 'POST',
+            data: {'foo':'bar'},
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        }).success(function(data, status, headers, config) {
+            $scope.data = data;
+        }).error(function(data, status, headers, config) {
+            $scope.status = status;
+        });
+
+    };
 
 };
 
-loginCtrl.$inject = ['$scope'];
+loginCtrl.$inject = ['$scope', '$http'];
