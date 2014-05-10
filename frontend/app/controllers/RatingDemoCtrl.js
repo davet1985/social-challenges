e41647ee1d37ff2674fe72786e1c992e6c8e78ed
Stephen Scott <s.p.scott@kainos.com>
2014-05-10 19:38:06 +0100

    Added rating post to front end, and in the backend calculate the average rating to one decimal place

--- frontend/app/controllers/RatingDemoCtrl.js
+++ frontend/app/controllers/RatingDemoCtrl.js
@@ -1,5 +1,6 @@
+/* jshint -W089 */
 
-var RatingDemoCtrl = function ($scope, $location) {
+var RatingDemoCtrl = function ($scope, $location, $http) {
 
 	$scope.max = 5;
 
@@ -11,9 +12,22 @@ var RatingDemoCtrl = function ($scope, $location) {
 
 	// do something with value
 	$scope.getValue = function (value, prevId, currentId, nextId) {
+        $http({
+            method  : 'POST',
+            url     : 'http://localhost:9292/rating/add',
+            data: {'userId': '1', 'againstTag': tagName, 'objectId': currentId, 'score': value},
+            transformRequest: function(obj) {
+                var str = [];
+                for(var p in obj) {
+                    str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                }
+                return str.join('&');
+            },
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+        });
 		$location.path('/'+tagName+'/'+nextId+'/'+currentId);
 	};
 
 };
 
-RatingDemoCtrl.$inject = ['$scope', '$location'];
+RatingDemoCtrl.$inject = ['$scope', '$location', '$http'];
