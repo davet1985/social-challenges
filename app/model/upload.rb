6dd85ff0ea5f8af4040f621a6a5146147a26b349
Stephen Scott <s.p.scott@kainos.com>
2014-05-12 22:09:22 +0100

    Added tags to return JSON in the web service

--- app/model/upload.rb
+++ app/model/upload.rb
@@ -21,7 +21,7 @@ class Upload
   end
 
   def to_json(*a)
-    {"id" => @id, "type" => @type, "file_name" => @file_name, "file_name" => "http://localhost:9292/upload/#{@id}/download", "userid" => @userid, "upload_datetime" => @upload_datetime, "averageScore" => @averageScore.round(1), "numOfRatings" => @numOfRatings, "title" => @title, "description" => @description, "tags" => @tags.to_json}.to_json(*a)
+    {"id" => @id, "type" => @type, "file_name" => @file_name, "file_name" => "http://localhost:9292/upload/#{@id}/download", "userid" => @userid, "upload_datetime" => @upload_datetime, "averageScore" => @averageScore.round(1), "numOfRatings" => @numOfRatings, "title" => @title, "description" => @description, "tags" => JSON.parse(@tags.to_json)}.to_json(*a)
   end
 
   def set_tags(tags)
@@ -30,7 +30,9 @@ class Upload
   
   def self.cast(results)
     uploads = Array.new
-    results.each { |r| uploads << Upload.new(r[2], r[3], r[4], r[5], r[9], r[10], r[1], r[0], r[6], r[7], r[8]) }
+    results.each { |r| uploads << upload = Upload.new(r[2], r[3], r[4], r[5], r[9], r[10], r[1], r[0], r[6], r[7], r[8]) 
+                                  upload.set_tags TagRepository.find_by_object_id(upload.id)
+                                  upload }
     uploads
   end
 
