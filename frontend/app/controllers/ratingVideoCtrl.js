0276c2f007eaa1af40b26df358320731bffa4d96
somme <s.sakounthong@kainos.com>
2014-05-18 15:14:29 +0100

    started videos, created youtube directive and ratingVideoView

---
+++ frontend/app/controllers/ratingVideoCtrl.js
@@ -0,0 +1,42 @@
+/* jshint -W089 */
+
+var ratingVideoCtrl = function ($scope, $location, $http, ratingService) {
+
+	$scope.ratings = ratingService.ratings;
+    
+    ratingService.getRatingData();
+
+    $scope.max = 5;
+
+    var tagName = $scope.getPageTag();
+
+    $scope.hoveringOver = function(value) {
+        $scope.overStar = value;
+    };
+
+    //youtube id of video
+    $scope.code = 'dYDGqmxMZFI';
+
+    // do something with value
+    /*
+    $scope.getValue = function (value, prevId, currentId, nextId) {
+        $http({
+            method  : 'POST',
+            url     : configService.API_END_POINT+'rating/add',
+            data: {'userId': '1', 'againstTag': tagName, 'objectId': currentId, 'score': value},
+            transformRequest: function(obj) {
+                var str = [];
+                for(var p in obj) {
+                    str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                }
+                return str.join('&');
+            },
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+        });
+        $location.path('/tag/'+tagName+'/'+nextId+'/'+currentId);
+    };
+    */
+
+};
+
+ratingVideoCtrl.$inject = ['$scope', '$location', '$http', 'ratingService'];
