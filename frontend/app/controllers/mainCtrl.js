f846531e47c43ed6acee42d73791251be97aa59f
somme <s.sakounthong@kainos.com>
2014-05-22 15:26:31 +0100

    added error checking for region api

--- frontend/app/controllers/mainCtrl.js
+++ frontend/app/controllers/mainCtrl.js
@@ -4,12 +4,20 @@ var mainCtrl = function ($scope, $location, $http) {
 		return $location.path().split('/')[2]||'Unknown';
     };
 
-    //get countrycode
-	$http({method: 'GET', url: 'http://freegeoip.net/json/'})
-	.success(function(data){
-		$scope.countryCode = data.country_code;
-	});
-	
+	//ckeck if country code exists - if not go get it
+	if (window.localStorage.getItem('CountryCode') !== null){
+		$scope.countryCode = window.localStorage.getItem('CountryCode');
+	} else{
+		$http({method: 'GET', url: 'http://freegeoip.net/json/'})
+		.success(function(data){
+			window.localStorage.setItem('CountryCode', data.country_code);
+			$scope.countryCode = window.localStorage.getItem('CountryCode');
+		})
+		.error(function(data) {
+			// ok lets just set a value
+			window.localStorage.setItem('CountryCode', 'us');
+		});
+	}
 
 };
 
