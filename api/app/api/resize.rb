9ad7ef52d3b818a68c35554ac769bcd4de061d91
somme <s.sakounthong@kainos.com>
2014-05-31 15:55:45 +0100

    added resize 3 sizes to example

--- api/app/api/resize.rb
+++ api/app/api/resize.rb
@@ -2,6 +2,10 @@ require 'rubygems'
 require 'rmagick'
 
 fname = '../../spec/uploads/cat.jpg'
-newFile = '../../spec/uploads/thumb.jpg'
+newFileLarge = '../../spec/uploads/resize_large.jpg'
+newFileMedium = '../../spec/uploads/resize_medium.jpg'
+newFileThumb = '../../spec/uploads/resize_thumb.jpg'
 
-Magick::Image::read(fname)[0].scale(0.5).write(newFile){|f| f.quality = 0.7 }
+Magick::Image::read(fname)[0].resize_to_fit(1200, 1200).write(newFileLarge){|f| f.quality = 0.7 }
+Magick::Image::read(fname)[0].resize_to_fit(600, 600).write(newFileMedium){|f| f.quality = 0.7 }
+Magick::Image::read(fname)[0].resize_to_fill(125, 125, Magick::CenterGravity).write(newFileThumb){|f| f.quality = 0.7 }
