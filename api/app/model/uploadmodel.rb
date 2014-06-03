ebdf84da71a6ad9e981b688a0d2baa197b65ae62
Stephen Scott <s.p.scott@kainos.com>
2014-06-03 21:30:42 +0100

    Added gravatar md5 hash to upload

--- api/app/model/uploadmodel.rb
+++ api/app/model/uploadmodel.rb
@@ -1,11 +1,12 @@
 require 'json'
+require 'digest/md5'
 
 class Uploadmodel
 
   attr_accessor :tags
-  attr_reader :upload_datetime, :type, :file_name, :original_file_name, :userid, :title, :description, :overallScore, :numOfRatings, :averageScore, :id
+  attr_reader :upload_datetime, :type, :file_name, :original_file_name, :userid, :title, :description, :overallScore, :numOfRatings, :averageScore, :id, :gravatar
 
-  def initialize(type, file_name, original_file_name, userid, title, description, upload_datetime = Time.now.to_s, id = nil, overallScore = 0, numOfRatings = 0, averageScore = 0)
+  def initialize(type, file_name, original_file_name, userid, title, description, upload_datetime = Time.now.to_s, id = nil, overallScore = 0, numOfRatings = 0, averageScore = 0, gravatar = "")
     @type = type
     @file_name = file_name
     @original_file_name = original_file_name
@@ -17,10 +18,12 @@ class Uploadmodel
     @overallScore = overallScore
     @numOfRatings = numOfRatings
     @averageScore = averageScore
+    @gravatar = gravatar
   end
 
   def to_json(*a)
-    {"id" => @id, "type" => @type, "file_name" => @file_name, "file_name" => "http://localhost:9292/upload/#{@id}/download", "userid" => @userid, "upload_datetime" => @upload_datetime, "overallScore" => @overallScore, "numOfRatings" => @numOfRatings, "title" => @title, "description" => @description, "tags" => JSON.parse(@tags.to_json)}.to_json(*a)
+    md5gravatar = Digest::MD5.hexdigest(@gravatar)
+    {"id" => @id, "type" => @type, "file_name" => @file_name, "file_name" => "http://localhost:9292/upload/#{@id}/download", "userid" => @userid, "upload_datetime" => @upload_datetime, "overallScore" => @overallScore, "numOfRatings" => @numOfRatings, "title" => @title, "description" => @description, "tags" => JSON.parse(@tags.to_json), "gravatar" => md5gravatar}.to_json(*a)
   end
 
 end
