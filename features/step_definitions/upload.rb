6e1c13800418137c25ec174ae1213d606a0892e8
Stephen Scott <s.p.scott@kainos.com>
2014-06-04 22:11:47 +0100

    Added cucumber test for rating picture

--- features/step_definitions/upload.rb
+++ features/step_definitions/upload.rb
@@ -25,3 +25,19 @@ Then(/^I should see be able to see the picture within the tags section$/) do
   page.should have_content('Giggs is a legend. One of the greatest players ever')
   page.should have_content('Top 20 in giggs')
 end
+
+Given(/^I check the current score of the picture$/) do
+  visit 'http://localhost:8000/#!/top/dog'
+  @likes = find_by_id('upload_9').text 
+end
+
+When(/^I like the picture$/) do
+  visit 'http://localhost:8000/#!/tag/dog/9/2'
+  find_by_id('likelot').click
+end
+
+Then(/^the picture total likes should have increased$/) do
+  click_link('Top 20 in dog')
+  @newLikes = find_by_id('upload_9').text
+  @newLikes.to_i.should == @likes.to_i + 2
+end
