fbecf827f2e29ee08d10e218b31483c794b4671a
somme <s.sakounthong@kainos.com>
2014-06-09 23:27:25 +0100

    frontend validation completed for forgot password

--- frontend/app/controllers/forgotCtrl.js
+++ frontend/app/controllers/forgotCtrl.js
@@ -1,39 +1,32 @@
 /* jshint -W089 */
 
-var forgotCtrl = function ($scope, $location, $http, configService, usernameService) {
+var forgotCtrl = function ($scope, $location, $http, configService, usernameService, $timeout, $anchorScroll) {
 
-    $scope.processForm = function() {
-        $http({
-            method  : 'POST',
-            url     : configService.API_END_POINT+'auth/forgot-password',
-            data: {'email': $scope.email},
-            transformRequest: function(obj) {
-                var str = [];
-                for(var p in obj) {
-                    str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
-                }
-                return str.join('&');
-            },
-            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-        })
+    $scope.processForm = function(isValid){
+        if (isValid){
+            $http({
+                method  : 'POST',
+                url     : configService.API_END_POINT+'auth/forgot-password',
+                data: {'email': $scope.email},
+                transformRequest: function(obj) {
+                    var str = [];
+                    for(var p in obj) {
+                        str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                    }
+                    return str.join('&');
+                },
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+            })
             .success(function(data) {
-				
-				//console.log(data.token);
-				//usernameService.setUsername(data.username, data.id, data.token);
-				
-				$location.path('/login');
-
-                // if (!data.success) {
-                //   // if not successful, bind errors to error variables
-                //     $scope.errorName = data.errors.name;
-                //     $scope.errorSuperhero = data.errors.superheroAlias;
-                // } else {
-                //   // if successful, bind success message to message
-                //     $scope.message = data.message;
-                // }
+                //$location.path('/login');
+                $scope.forgotSuccessMessage = data.status;
             });
+        
+        } else{
+            $scope.submittedError = true;
+            $anchorScroll('top');
+        }
     };
-
 };
 
-forgotCtrl.$inject = ['$scope', '$location', '$http', 'configService', 'usernameService'];
+forgotCtrl.$inject = ['$scope', '$location', '$http', 'configService', 'usernameService', '$timeout', '$anchorScroll'];
