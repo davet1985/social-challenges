004548ef659153e0eff43fab01ae5f09ad5769ff
Stephen Scott <s.p.scott@kainos.com>
2014-06-13 22:21:12 +0100

    swapped mode and type in the uploads api to be consistent with the tag api

--- frontend/app/controllers/userRecentCtrl.js
+++ frontend/app/controllers/userRecentCtrl.js
@@ -1,6 +1,6 @@
 var userRecentCtrl = function ($scope, userCloudService) {
 
-	userCloudService.getTagCloudData('tag', 'recent', 'all', '5').then(function(d) {
+	userCloudService.getTagCloudData('user', 'recent', 'all', '5').then(function(d) {
 		$scope.tagCloud = d.data;
 	});
 
