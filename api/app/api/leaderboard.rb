f41e3913b48f7b38c228e8e10d7743709f177906
Stephen Scott <s.p.scott@kainos.com>
2014-06-17 22:16:54 +0100

    fixed issue with top 20 and started api for trophies search

--- api/app/api/leaderboard.rb
+++ api/app/api/leaderboard.rb
@@ -4,6 +4,7 @@ require 'rack/contrib'
 
 require_relative './../model/uploadmodel'
 require_relative './../repository/leaderboard_repository'
+require_relative './../repository/tag_repository'
 
 module SocialChallenges
 
@@ -14,6 +15,28 @@ module SocialChallenges
 
     get '/:tagName/:numberToGet' do
       JSON.parse(LeaderboardRepository.get_leaderboard_bytagname(params[:tagName], params[:numberToGet]).to_json)
+    end
+    
+    get '/search/:tagName/:numberToGet' do
+      tags = TagRepository.alphabetic(params[:tagName], 300000, 'tag')
+      json_new = []
+      tags.each { |tag|
+        objects = LeaderboardRepository.get_leaderboard_bytagname(tag[2], params[:numberToGet])
+        json_object = []
+        objects.each_with_index {|object, index|
+          json_object.push(JSON.parse(
+            {
+              "number" => index + 1,
+              "object" => JSON.parse(object.to_json)
+            }.to_json
+          )) 
+        }
+        json_new.push(JSON.parse({
+          "tag" => tag[2],
+          "objects" => JSON.parse(json_object.to_json)
+        }.to_json))
+      }
+      JSON.parse(json_new.to_json)
     end
 
   end
