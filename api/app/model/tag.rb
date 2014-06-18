1bd40c4bebd357acea90dd6240e65fcd37c4ca16
Stephen Scott <s.p.scott@kainos.com>
2014-06-18 22:04:39 +0100

    Backend urls are now configurable in a yaml file

--- api/app/model/tag.rb
+++ api/app/model/tag.rb
@@ -1,8 +1,11 @@
 require 'json'
+require 'yaml'
 
 class Tag
 
   attr_reader :tagName, :userId, :id, :tag_datetime, :numOfObjects, :type
+  
+  CONFIG = YAML.load_file("./config/config.yml") unless defined? CONFIG
 
   def initialize(tagName, userId, type = 'tag', id = nil, tag_datetime = Time.now.to_s, numOfObjects = 0)
     @tagName = tagName
@@ -19,25 +22,25 @@ class Tag
   
   def self.returnJSON(currentObject, previousObject, nextObject)
     [{
-      "current"       => JSON.parse(currentObject.to_json), "previous" => {"id" => previousObject.id, "original_file_name" => previousObject.original_file_name, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name_medium" => "http://localhost:9292/upload/#{previousObject.id}/download/medium", "file_name" => "http://localhost:9292/upload/#{previousObject.id}/download"}, "next" => {"id" => nextObject.id, "original_file_name" => nextObject.original_file_name,  "type" => nextObject.type, "file_name_medium" => "http://localhost:9292/upload/#{nextObject.id}/download/medium", "overallScore" => nextObject.overallScore, "file_name" => "http://localhost:9292/upload/#{nextObject.id}/download"}
+      "current"       => JSON.parse(currentObject.to_json), "previous" => {"id" => previousObject.id, "original_file_name" => previousObject.original_file_name, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name_medium" => "http://#{CONFIG['backend_url']}/upload/#{previousObject.id}/download/medium", "file_name" => "http://#{CONFIG['backend_url']}/upload/#{previousObject.id}/download"}, "next" => {"id" => nextObject.id, "original_file_name" => nextObject.original_file_name,  "type" => nextObject.type, "file_name_medium" => "http://#{CONFIG['backend_url']}/upload/#{nextObject.id}/download/medium", "overallScore" => nextObject.overallScore, "file_name" => "http://#{CONFIG['backend_url']}/upload/#{nextObject.id}/download"}
       }].to_json
   end
   
   def self.returnJSONNoNext(currentObject, previousObject)
     [{
-      "current"       => JSON.parse(currentObject.to_json), "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "http://localhost:9292/upload/#{previousObject.id}/download","file_name_medium" => "http://localhost:9292/upload/#{previousObject.id}/download/medium", "original_file_name" => previousObject.original_file_name}, "next" => {"type" => 'end'}
+      "current"       => JSON.parse(currentObject.to_json), "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "http://#{CONFIG['backend_url']}/upload/#{previousObject.id}/download","file_name_medium" => "http://#{CONFIG['backend_url']}/upload/#{previousObject.id}/download/medium", "original_file_name" => previousObject.original_file_name}, "next" => {"type" => 'end'}
       }].to_json
   end
   
   def self.returnJSONNoPrevious(currentObject, nextObject)
     [{
-      "current"       => JSON.parse(currentObject.to_json), "next" => {"id" => nextObject.id, "type" => nextObject.type, "overallScore" => nextObject.overallScore, "file_name" => "http://localhost:9292/upload/#{nextObject.id}/download", "file_name_medium" => "http://localhost:9292/upload/#{nextObject.id}/download/medium", "original_file_name" => nextObject.original_file_name}
+      "current"       => JSON.parse(currentObject.to_json), "next" => {"id" => nextObject.id, "type" => nextObject.type, "overallScore" => nextObject.overallScore, "file_name" => "http://#{CONFIG['backend_url']}/upload/#{nextObject.id}/download", "file_name_medium" => "http://#{CONFIG['backend_url']}/upload/#{nextObject.id}/download/medium", "original_file_name" => nextObject.original_file_name}
       }].to_json
   end
   
   def self.returnJSONPreviousRandomTag(previousObject, randomTag)
     [{
-      "randomTag" => randomTag, "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "http://localhost:9292/upload/#{previousObject.id}/download","file_name_medium" => "http://localhost:9292/upload/#{previousObject.id}/download/medium", "original_file_name" => previousObject.original_file_name}, "next" => {"type" => 'end'}
+      "randomTag" => randomTag, "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "http://#{CONFIG['backend_url']}/upload/#{previousObject.id}/download","file_name_medium" => "http://#{CONFIG['backend_url']}/upload/#{previousObject.id}/download/medium", "original_file_name" => previousObject.original_file_name}, "next" => {"type" => 'end'}
       }].to_json
   end
   
